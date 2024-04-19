2ceb93272eb62dba3cb99f02adaca4d270d0ad67
JKMILK <85205003+JKMILK@users.noreply.github.com>
2024-04-19 14:12:08 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Punchline), music.PlaybackMode.InBackground)
+})
+basic.showIcon(IconNames.StickFigure)
+basic.forever(function () {
+	
+})
